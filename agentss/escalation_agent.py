3352838e58666ff1bb1c2987ca6355138a0f7a4e
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2025-07-18 20:42:09 +0500

    Update escalation_agent.py

--- agentss/escalation_agent.py
+++ agentss/escalation_agent.py
@@ -1,5 +1,14 @@
-from agents.tool import function_tool
+# from agents.tool import function_tool
 
-@function_tool
-def EscalationAgent(issue: str = "emergency") -> str:
-    return f" your have  '{issue}' kindly contact to doctor.i cannot help more on this "
+# @function_tool
+# def EscalationAgent(issue: str = "emergency") -> str:
+#     return f" your have  '{issue}' kindly contact to doctor.i cannot help more on this "
+
+#now we use make special agent
+
+from agents import Agent
+
+escalation_agent = Agent(
+    name="Escalation Agent",
+    instructions="You are a human support assistant. Help the user and prepare to hand them off to a real human if needed."
+)
