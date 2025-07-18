9c7371fc58c99d7e843c58c5cb72d981efc2ae00
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2025-07-18 19:50:14 +0500

    Update guardrails.py

--- guardrails.py
+++ guardrails.py
@@ -1,13 +1,35 @@
 from pydantic import BaseModel
+from agents import (
+    Agent,
+    GuardrailFunctionOutput,
+    InputGuardrailTripwireTriggered,
+    RunContextWrapper,
+    Runner,
+    input_guardrail,
+    output_guardrail
+)
+from context import UserSessionContext
 
-class GoalInput(BaseModel):
-    goal_type: str  
-    quantity: float
-    metric: str     
-    duration: str    
 
-class MealPlanOutput(BaseModel):
-    meals: list[str]
+class GoalInputGuardrailOutput(BaseModel):
+    is_valid: bool
+    reason: str
 
-class WorkoutPlanOutput(BaseModel):
-    plan: dict
+
+goalagent = Agent(
+    name="Goal checkagent",
+    instructions="Check if the input goal is correct",
+    output_type=GoalInputGuardrailOutput,
+)
+
+@input_guardrail
+async def validate_goal_input(
+    ctx: RunContextWrapper[UserSessionContext],
+    agent: Agent,
+    input: str
+) -> GuardrailFunctionOutput:
+    result = await Runner.run(goalagent, input, context=ctx.context)
+    return GuardrailFunctionOutput(
+        output_info=result.final_output,
+        tripwire_triggered=not result.final_output.is_valid
+    )
