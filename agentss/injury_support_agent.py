b75d6f1c9af26f29e6aa3461ed0db5cf8804d31b
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2025-07-18 20:42:54 +0500

    Update injury_support_agent.py

--- agentss/injury_support_agent.py
+++ agentss/injury_support_agent.py
@@ -1,6 +1,16 @@
-from agents.tool import function_tool
+# from agents.tool import function_tool
 
-@function_tool
-def InjurySupportAgent(injury: str = "knee pain") -> str:
-    return f" your {injury}, if u have lot of pain , please go to hospital and see doctor , if u have little pain , please try to rest and do some exercise to recover "
+# @function_tool
+# def InjurySupportAgent(injury: str = "knee pain") -> str:
+#     return f" your {injury}, if u have lot of pain , please go to hospital and see doctor , if u have little pain , please try to rest and do some exercise to recover "
 
+#now we make special agent
+from agents import Agent
+
+injury_agent = Agent(
+    name="Injury Support Agent",
+    instructions=(
+        "You are a fitness specialist who helps users with physical limitations "
+        "or injuries. Recommend modified workouts and precautions.u are expert on this"
+    )
+)
