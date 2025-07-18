76512537cb084fb41e7cfda67afd45913bfd6e3d
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2025-07-18 20:43:41 +0500

    Update nutrition_expert_agent.py

--- agentss/nutrition_expert_agent.py
+++ agentss/nutrition_expert_agent.py
@@ -1,6 +1,17 @@
-from agents.tool import function_tool
+# from agents.tool import function_tool
 
-@function_tool
-def NutritionExpertAgent(query: str = "daily protein needs") -> str:
-    return f" As a nutritionist, i suggest you consume {query} to maintain a healthy lifestyle."
+# @function_tool
+# def NutritionExpertAgent(query: str = "daily protein needs") -> str:
+#     return f" As a nutritionist, i suggest you consume {query} to maintain a healthy lifestyle."
 
+#now we make special agent 
+
+from agents import Agent
+
+nutrition_agent = Agent(
+    name="Nutrition Expert Agent",
+    instructions=(
+        "You are a nutrition expert. Help users with dietary needs as well as "
+        "such as diabetes, food allergies, or medical conditions.much more"
+    )
+)
